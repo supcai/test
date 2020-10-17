a4e7d4caee460ddb444cd15b01dbaae1425e4f37
supercai <72127759+supercai-pro@users.noreply.github.com>
2020-10-17 23:40:54 +0800

    Add files via upload
    test file

---
+++ test.js
@@ -0,0 +1,21 @@
+(function () {
+	var test = function () {
+
+		window.setInterval(function () {
+	
+			document.getElementsByClassName('ChatSend-txt')[0].focus()
+		
+	document.getElementsByClassName('ChatSend-txt')[0].value = '666666'
+			
+document.getElementsByClassName('ChatSend-button')[0].click()
+		
+},20500)
+
+	}
+	window.test = test
+	test()
+})()
+
+
+
+
