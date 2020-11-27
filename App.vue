01b5057acc13fa7b94576e289ae4517fdbe7e05a
supercai <72127759+supercai-pro@users.noreply.github.com>
2020-11-27 14:15:09 +0800

    私有项目转移

---
+++ App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <el-container direction="vertical">
+      <page-header :layout="this.$store.state.layout"></page-header>
+      <el-container class="main-container">
+        <page-side :layout="this.$store.state.layout"></page-side>
+        <el-main>
+          <router-view/>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import pageHeader from '@/components/header'
+import pageSide from '@/components/side'
+export default {
+  name: 'App',
+  components: {
+    'page-header': pageHeader,
+    'page-side': pageSide
+  }
+}
+</script>
+<style></style>
